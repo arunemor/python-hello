125fb8bb493318d3a48ea51e10d84f04bb69cc1e
arunemor <cloudemor2025@gmail.com>
2025-09-19 11:00:14 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,12 @@
+from flask import Flask
+
+# Create a Flask app
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+    return "Hello, World!"
+
+if __name__ == "__main__":
+    # Run on port 5000 (you can change it)
+    app.run(host="0.0.0.0", port=5000, debug=True)
