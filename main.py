acdff313786d1f9ed4600284c138e085d1710a05
arunemor <cloudemor2025@gmail.com>
2025-09-23 19:14:35 +0530

    some update

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello, Arun and Gaurav Sir. How are you?"
+    return "Hello, When creating or updating the launch template or launch configuration used by your ASG, specify the IAM role created in the previous step as the This ensures that all instances launched by the ASG will automatically assume this role and its associated permissions."
 
 if __name__ == "__main__":
     # Run on port 5000 (you can change it)
