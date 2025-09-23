b6ea28093e5742a73692a8893e5baa60b32be197
arunemor <cloudemor2025@gmail.com>
2025-09-23 14:57:04 +0530

    final check

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello, Arun and Gaurav Sir. How are you."
+    return "Hello, Arun and Gaurav Sir. How are you?"
 
 if __name__ == "__main__":
     # Run on port 5000 (you can change it)
